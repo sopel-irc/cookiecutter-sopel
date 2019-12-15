4d53a02d395154b4fa79921b025d43d2da4c3e1a
dgw <dgw@technobabbl.es>
2019-12-15 01:41:43 -0600

    Revamp how requirements work
    We should encourage developers to use setup.cfg for new plugins. Dealing
    with requirements.txt files is old school.

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -18,15 +18,7 @@ with open('README.md') as readme_file:
 with open('NEWS') as history_file:
     history = history_file.read()
 
-with open('requirements.txt') as requirements_file:
-    requirements = [req for req in requirements_file.readlines()]
-
-with open('dev-requirements.txt') as dev_requirements_file:
-    dev_requirements = [req for req in dev_requirements_file.readlines()]
-
 
 setup(
     long_description=readme + '\n\n' + history,
-    install_requires=requirements,
-    tests_require=dev_requirements,
 )
