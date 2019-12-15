9ea64994640b2dd94f886342dc928dda5bc92ad3
dgw <dgw@technobabbl.es>
2019-12-15 01:46:30 -0600

    Slim down to one plugin file
    Similar to tests, most plugin devs probably won't ever bother splitting
    their code into multiple files. Those who do are probably used to it,
    and don't need our help figuring out the file structure.
    
    Cutting down to a single file *might* also make reloading packaged
    plugins work better until a proper solution can be implemented in Sopel
    itself (but *if* it does, that's merely a side benefit of this change).

--- {{cookiecutter.repo_name}}/sopel_{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/sopel_{{cookiecutter.module_name}}/__init__.py
@@ -5,9 +5,17 @@
 """
 from __future__ import unicode_literals, absolute_import, division, print_function
 
-from .{{cookiecutter.module_name}} import *
+from sopel import module
 
-__author__ = '{{cookiecutter.full_name}}'
-__email__ = '{{cookiecutter.email}}'
-__version__ = '{{cookiecutter.version}}'
 
+def configure(config):
+    pass
+
+
+def setup(bot):
+    pass
+
+
+@module.commands('helloworld')
+def hello_world(bot, trigger):
+    bot.say('Hello, world!')
